5edd90f5b50ae7d3a405d01c202fdec51672b0ff
son426 <coghks0426@naver.com>
2026-01-07 02:45:24 +0900

    ver 0.1.9

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,11 +1,7 @@
-export {
-  A as AutoApplyConfig,
-  R as ResponsiveImageData,
-  V as ViteImageConfig,
-} from "./types-B08JIQxT.js";
-export { ImageProps } from "./react/index.js";
-export { ViteImagePluginOptions, viteImage } from "./plugin/index.js";
-import "vite-imagetools";
-import "react/jsx-runtime";
-import "react";
-import "vite";
+export { A as AutoApplyConfig, R as ResponsiveImageData, V as ViteImageConfig } from './types-B08JIQxT.js';
+export { ImageProps } from './react/index.js';
+export { ViteImagePluginOptions, viteImage } from './plugin/index.js';
+import 'vite-imagetools';
+import 'react/jsx-runtime';
+import 'react';
+import 'vite';
